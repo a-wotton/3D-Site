8e174a5638428961e55ccdd649a4d96c81770ff3
a-wotton <a_wotton@fanshaweonline.ca>
2025-03-20 11:55:32 -0400

    started hoempage

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'lg': '1024px'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
